cfed90165b80bacef17ecf0d237f6069702fc07d
mikebrumfield <mikebrumfield30@gmail.com>
2021-10-16 22:05:53 -0500

    init commit

---
+++ AcceptedImages/src/test/java/com/synthesis/AcceptedImages/AcceptedImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synthesis.AcceptedImages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcceptedImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
